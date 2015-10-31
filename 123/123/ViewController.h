5154ae37c0f2dfc24ba56c6bdb2019a117bcbbba
guominjiangkangyun <941515035@qq.com>
2015-10-31 22:57:40 +0800

    sad

---
+++ 123/123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  123
+//
+//  Created by niuhenglei on 15/10/31.
+//  Copyright (c) 2015年 niuhenglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 123/123/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  123
+//
+//  Created by niuhenglei on 15/10/31.
+//  Copyright (c) 2015年 niuhenglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+        @autoreleasepool {
+            return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        }
+}
